0eb004d87bbab40dbc307d5e1ba870552600f01f
PURUSHOTH <57170621+purushoth0028@users.noreply.github.com>
2022-10-17 08:34:37 +0530

    Delete rubick directory

--- rubick/Backend/TableController.js
+++
@@ -1,6 +0,0 @@
-const TableModel = require("../model/TableModel");
-
-module.exports.getToDo = async (req, res) => {
-    const todo = await TableModel.find();
-    res.send(todo);
-}
--- rubick/Backend/TableRoute.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-
-const { getToDo } = require("../Controllers/TableController");
-
-const router = Router();
-
-router.get("/get-todo", getToDo);
-module.exports=router;
